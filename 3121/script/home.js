7299a02f978f888f35be06c96bc9d0474f41f262
find-3121-and-do-not-see-the-code <102374922+find-3121-and-do-not-see-the-code@users.noreply.github.com>
2022-04-13 23:27:37 +0000

    Add files via upload

---
+++ 3121/script/home.js
@@ -0,0 +1,12 @@
+const burger=document.querySelector(".lol")
+const asj=document.getElementById("asj")
+
+burger.addEventListener('click',()=>
+{
+  burger.classList.toggle("li")
+  asj.classList.toggle("on")
+}
+)
+
+
+
---
+++ 3121/script/join.js
@@ -0,0 +1,2 @@
+function t(){
+  if(document.getElementById("gt").value==btd || document.getElementById("gt").value==v){alert("https://ibb.co/kMFjrRf")}else{alert("find the image")}}
